1ba873a050953459d34376a2371adc442ef90504
aceberrg <161969265+aceberrg@users.noreply.github.com>
2024-03-19 10:34:35 +0300

    я не отдам хлеб я голоден
    я каменщик работаю три дня без зарплаты заберите свой металлолом

--- lab20.py
+++ lab20.py
@@ -5,7 +5,7 @@ app = QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QWidget() 
 window.setWindowTitle("Первая программа на PyQt") 
 window.setWindowFlag(QtCore.Qt.WindowCloseButtonHint, False)
-window.setFixedSize(300, 70)
+window.setFixedSize(300, 400)
 
 label = QtWidgets.QLabel("<center>Привет, мир!</center>") 
 
@@ -19,3 +19,4 @@ btnQuit.clicked.connect(app.quit)
 
 window.show() 
 sys.exit(app.exec_())
+print('z t,e fkb,f,e')
